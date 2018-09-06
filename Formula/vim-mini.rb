08f70c56f274db06dbb55175048b8515cebb270a
Julien Feltesse <julien.feltesse@gmail.com>
2018-09-06 23:40:55 +0900

    vim 8.1.0300

--- Formula/vim-mini.rb
+++ Formula/vim-mini.rb
@@ -2,8 +2,8 @@ class VimMini < Formula
   desc "Minimalistic Vim formula with optional dependencies"
   homepage "https://www.vim.org/"
   # vim should only be updated every 50 releases on multiples of 50
-  url "https://github.com/vim/vim/archive/v8.1.0150.tar.gz"
-  sha256 "d27812bc4fd0a901e0f3c082ef798cfad10f251adcfc6dec2ca8fcea34b2da17"
+  url "https://github.com/vim/vim/archive/v8.1.0300.tar.gz"
+  sha256 "e3064dbc51140c68d7b064c247787510ee6178a6a19d448b9ab976c7cf300c28"
   head "https://github.com/vim/vim.git"
 
   option "with-override-system-vi", "Override system vi"
